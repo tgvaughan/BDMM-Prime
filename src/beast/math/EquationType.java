8513468b6615755758e6aa75af72ea73d86f56b8
jscire <scirejeremie@gmail.com>
2016-08-05 18:02:05 +0200

    Added missing files from package 'math'

---
+++ src/beast/math/EquationType.java
@@ -0,0 +1,10 @@
+package beast.math;
+
+import beast.core.Description;
+
+@Description("This enumeration represents the types of equations or systems of equations for which the scale/unscale methods in class SmallNumberScaler are defined.")
+public enum EquationType {
+
+	EquationOnP,
+	EquationOnGe;
+}
