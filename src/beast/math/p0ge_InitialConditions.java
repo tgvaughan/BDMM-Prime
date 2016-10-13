7b46b4ec30b7657348da900d824ac0cb1f42af10
jscire <scirejeremie@gmail.com>
2016-10-13 15:44:30 +0200

    Included the p0ge_initialconditions class in the repo

---
+++ src/beast/math/p0ge_InitialConditions.java
@@ -0,0 +1,35 @@
+package beast.math;
+
+import beast.core.Description;
+
+@Description("This type contains both sets of initial conditions for both equation types: p and ge")
+public class p0ge_InitialConditions {
+	
+	int dimension;
+	public SmallNumber[] conditionsOnG;
+	public double[] conditionsOnP;
+	
+	public p0ge_InitialConditions(double[] pcond, SmallNumber[] gcond) {
+		if(pcond.length != gcond.length) {
+			throw new RuntimeException("Incorrect initialization: difference of size between conditionsOnG and conditionsOnP");
+		}
+		dimension = pcond.length;
+		conditionsOnP = pcond;
+		conditionsOnG = gcond;
+	}
+	
+	public p0ge_InitialConditions() {		
+		dimension = 1;
+		conditionsOnP = new double[] {0};
+		conditionsOnG = new SmallNumber[] {new SmallNumber()};
+	}
+	
+	public double[] getConditionsOnP(){
+		return this.conditionsOnP;
+	}
+	
+	public SmallNumber[] getConditionsOnG(){
+		return this.conditionsOnG;
+	}
+
+}
