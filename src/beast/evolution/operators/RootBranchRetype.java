e66af982e9b80d50d4f13727b9118fb42c1f8e65
Denise Kuehnert <denise.kuehnert@gmail.com>
2018-07-06 09:13:07 +0200

    access mu as Function

--- src/beast/evolution/operators/RootBranchRetype.java
+++ src/beast/evolution/operators/RootBranchRetype.java
@@ -77,7 +77,7 @@ public class RootBranchRetype extends RandomRetypeOperator {
      */
     protected double retypeBranch(MultiTypeRootBranch srcNode) {
 
-        double mu = muInput.get();
+        double mu = muInput.get().getArrayValue();
 
         double t_srcNode = mtTree.getRoot().getHeight();
         double t_srcNodeParent = originInput.get().getValue();
@@ -129,7 +129,7 @@ public class RootBranchRetype extends RandomRetypeOperator {
      */
     protected double getBranchTypeProb(MultiTypeRootBranch srcNode) {
 
-        double mu = muInput.get();
+        double mu = muInput.get().getArrayValue();
         int n = srcNode.getChangeCount();
         int N = migModel.getNTypes();
 
