c0c7d62e1f1ae14fd7b5441565907bb16b46d304
Tim Vaughan <tgvaughan@gmail.com>
2019-01-08 17:12:15 +0100

    Updated unit tests for getIntervalIndex()

--- test/bdmm/util/UtilsTest.java
+++ test/bdmm/util/UtilsTest.java
@@ -10,12 +10,12 @@ public class UtilsTest {
 
         double[] startTimes = {0.0, 0.5, 0.9};
 
-        Assert.assertEquals(-1, Utils.getIntervalIndex(-3.0, startTimes));
+        Assert.assertEquals(0, Utils.getIntervalIndex(-3.0, startTimes));
         Assert.assertEquals(0, Utils.getIntervalIndex(0.0, startTimes));
         Assert.assertEquals(0, Utils.getIntervalIndex(0.1, startTimes));
-        Assert.assertEquals(1, Utils.getIntervalIndex(0.5, startTimes));
+        Assert.assertEquals(0, Utils.getIntervalIndex(0.5, startTimes));
         Assert.assertEquals(1, Utils.getIntervalIndex(0.7, startTimes));
-        Assert.assertEquals(2, Utils.getIntervalIndex(0.9, startTimes));
+        Assert.assertEquals(1, Utils.getIntervalIndex(0.9, startTimes));
         Assert.assertEquals(2, Utils.getIntervalIndex(1.0, startTimes));
     }
 }
