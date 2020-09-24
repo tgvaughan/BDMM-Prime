90a2e1f8d843ead4cc4cb7d957de42129bac3f56
Tim Vaughan <timv@ughan.xyz>
2020-09-24 10:19:33 +0200

    Made trajectory and typed tree loggers optional.

---
+++ src/bdmmprime/util/OptionalLogger.java
@@ -0,0 +1,51 @@
+package bdmmprime.util;
+
+import beast.core.Description;
+import beast.core.Input;
+import beast.core.Logger;
+
+import java.io.IOException;
+
+@Description("A Logger which can be disabled in BEAUti via a checkbox.")
+public class OptionalLogger extends Logger {
+
+    public Input<Boolean> enableLoggerInput = new Input<>("enableLogger",
+            "If false, logger produces no output. Default is false.", false);
+
+    @Override
+    public void initAndValidate() {
+        if (enableLoggerInput.get())
+            super.initAndValidate();
+    }
+
+    @Override
+    public void init() throws IOException {
+        if (enableLoggerInput.get())
+            super.init();
+    }
+
+    @Override
+    public void log(long sampleNr) {
+        if (enableLoggerInput.get())
+            super.log(sampleNr);
+    }
+
+    @Override
+    public void close() {
+        if (enableLoggerInput.get())
+            super.close();
+    }
+
+    @Override
+    public boolean isLoggingToStdout() {
+
+        // In the case that the logger is not enabled, report that we're
+        // logging to stdout.  This is a hack to avoid problems with
+        // missing files and state resumption.
+
+        if (enableLoggerInput.get())
+            return super.isLoggingToStdout();
+        else
+            return true;
+    }
+}
