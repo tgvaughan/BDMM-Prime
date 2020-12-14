ee214d32f38579749efb6a3175ec4c998cf33152
Cecilia Valenzuela <ceci.vale.a@gmail.com>
2020-12-14 11:56:47 +0100

    wip working several predictors

--- src/bdmmprime/util/priors/MigRateGLM.java
+++ src/bdmmprime/util/priors/MigRateGLM.java
@@ -6,8 +6,12 @@ import beast.core.Input;
 
 public class MigRateGLM extends CalculationNode implements Function {
 
-    public Input<Function> covariateMatrixInput = new Input<>("covariateMatrix",
-            "Matrix of covariate, e.g. numbers of flights between different locations",
+//    public Input<Function> covariateMatrixInput = new Input<>("covariateMatrix",
+//            "Matrix of covariate, e.g. numbers of flights between different locations",
+//            Input.Validate.REQUIRED);
+
+    public Input<Function> covariateListInput = new Input<>("covariateList",
+            "List of matrix of covariates, e.g. numbers of flights between different locations",
             Input.Validate.REQUIRED);
 
     public Input<Function> scaleParamInput = new Input<>("scaleParam",
@@ -16,26 +20,36 @@ public class MigRateGLM extends CalculationNode implements Function {
     public Input<Function> errorInput = new Input<>("errorTerm",
             "Error term.", Input.Validate.OPTIONAL);
 
-    Function covariateMatrix, scaleParam, errorTerm;
+    int covariateSize;
+
+    Function covariateList, scaleParam, errorTerm;
+//    Function covariateMatrix, scaleParam, errorTerm;
 
     @Override
     public void initAndValidate() {
-        covariateMatrix = covariateMatrixInput.get();
+//        covariateMatrix = covariateMatrixInput.get();
+        covariateList = covariateListInput.get();
         scaleParam = scaleParamInput.get();
         errorTerm = errorInput.get();
+        covariateSize = covariateList.getDimension()/(scaleParam.getDimension());
     }
 
     @Override
     public int getDimension() {
-        return covariateMatrix.getDimension();
+        return covariateSize;
+//        return covariateList.getDimension()/(scaleParam.getDimension());
     }
 
     @Override
     public double getArrayValue(int i) {
-        double lograte = scaleParam.getArrayValue()*covariateMatrix.getArrayValue(i);
+        double lograte = 0;
+//        double lograte = scaleParam.getArrayValue()*covariateMatrix.getArrayValue(i);
+        for (int j = 0; j < scaleParam.getDimension(); j++){
+            lograte += scaleParam.getArrayValue(j)*covariateList.getArrayValue(j * covariateSize + i);
+        }
 
-        if (errorTerm!=null)
-            lograte += errorTerm.getArrayValue(i);
+//        if (errorTerm!=null)
+//            lograte += errorTerm.getArrayValue(i);
 
         return Math.exp(lograte);
     }
