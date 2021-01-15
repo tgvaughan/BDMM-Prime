7b721624e0cac9a19d4d7bbdea67800d42062cd2
Cecilia Valenzuela <ceci.vale.a@gmail.com>
2021-01-15 18:00:43 +0100

    GLM with global scaler

--- src/bdmmprime/util/priors/MigRateGLM.java
+++ src/bdmmprime/util/priors/MigRateGLM.java
@@ -3,6 +3,7 @@ package bdmmprime.util.priors;
 import beast.core.CalculationNode;
 import beast.core.Function;
 import beast.core.Input;
+import beast.core.parameter.RealParameter;
 
 public class MigRateGLM extends CalculationNode implements Function {
 
@@ -10,26 +11,32 @@ public class MigRateGLM extends CalculationNode implements Function {
             "List of matrix of covariates, e.g. numbers of flights between different locations",
             Input.Validate.REQUIRED);
 
-    public Input<Function> scaleParamInput = new Input<>("scaleParam",
-            "Scale parameter.", Input.Validate.REQUIRED);
+    public Input<Function> scalerParamInput = new Input<>("scalerParam",
+            "Scaler parameter.", Input.Validate.REQUIRED);
 
     public Input<Function> indicatorParamInput = new Input<>("indicatorParam",
             "Indicator parameter.", Input.Validate.REQUIRED);
 
-    public Input<Function> errorInput = new Input<>("errorTerm",
-            "Error term.", Input.Validate.OPTIONAL);
+    public Input<RealParameter> globalScalerParamInput = new Input<>("globalScalerParam",
+            "Global scaler parameter.", Input.Validate.OPTIONAL);
 
+//    public Input<Function> errorInput = new Input<>("errorTerm",
+//            "Error term.", Input.Validate.OPTIONAL);
+
+    Function covariateList, scalerParam, indicatorParam;
+    double globalScalerParam;
     int covariateSize;
-
-    Function covariateList, scaleParam, indicatorParam, errorTerm;
 
     @Override
     public void initAndValidate() {
         covariateList = covariateListInput.get();
-        scaleParam = scaleParamInput.get();
+        scalerParam = scalerParamInput.get();
         indicatorParam = indicatorParamInput.get();
-        errorTerm = errorInput.get();
-        covariateSize = covariateList.getDimension()/(scaleParam.getDimension());
+
+        globalScalerParam = globalScalerParamInput.get().getArrayValue();
+//        errorTerm = errorInput.get();
+
+        covariateSize = covariateList.getDimension()/(scalerParam.getDimension());
     }
 
     @Override
@@ -40,15 +47,15 @@ public class MigRateGLM extends CalculationNode implements Function {
     @Override
     public double getArrayValue(int i) {
         double lograte = 0;
-        for (int j = 0; j < scaleParam.getDimension(); j++) {
+        for (int j = 0; j < scalerParam.getDimension(); j++) {
             if (indicatorParam.getArrayValue(j) > 0.0) {
-                lograte += scaleParam.getArrayValue(j) * covariateList.getArrayValue(j * covariateSize + i);
+                lograte += scalerParam.getArrayValue(j) * covariateList.getArrayValue(j * covariateSize + i);
             }
         }
 
 //        if (errorTerm!=null)
 //            lograte += errorTerm.getArrayValue(i);
 
-        return Math.exp(lograte);
+        return globalScalerParam * Math.exp(lograte);
     }
 }
