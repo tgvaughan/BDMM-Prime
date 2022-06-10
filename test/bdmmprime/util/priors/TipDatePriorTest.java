43684daab2ab8c71a95104ad35ef1f7b881e824f
Tim Vaughan <timv@ughan.xyz>
2022-06-10 16:14:27 +0200

    Removed accidental dependence on feast.

--- test/bdmmprime/util/priors/TipDatePriorTest.java
+++ test/bdmmprime/util/priors/TipDatePriorTest.java
@@ -1,12 +1,10 @@
 package bdmmprime.util.priors;
 
 import beast.core.parameter.RealParameter;
-import beast.evolution.alignment.Alignment;
 import beast.evolution.alignment.Taxon;
 import beast.evolution.alignment.TaxonSet;
 import beast.evolution.tree.TraitSet;
 import beast.evolution.tree.Tree;
-import feast.fileio.TraitSetFromTaxonSet;
 import junit.framework.Assert;
 import org.junit.Test;
 
@@ -20,37 +18,46 @@ public class TipDatePriorTest {
     public void test() {
 
         List<Taxon> taxonList = new ArrayList<>();
-        taxonList.add(new Taxon("t1|2020-05-15|2020-05-01|2020-06-01"));
-        taxonList.add(new Taxon("t2|2020-02-15|2020-02-01|2020-03-01"));
-        taxonList.add(new Taxon("t3|2021-05-15|2021-05-01|2021-06-01"));
-        taxonList.add(new Taxon("t4|2020-05-15|2020-05-14|2020-05-15"));
-        taxonList.add(new Taxon("t5|2020-05-15|2020-05-14|2020-05-15"));
+        taxonList.add(new Taxon("t1"));
+        taxonList.add(new Taxon("t2"));
+        taxonList.add(new Taxon("t3"));
+        taxonList.add(new Taxon("t4"));
+        taxonList.add(new Taxon("t5"));
         TaxonSet taxonSet = new TaxonSet(taxonList);
 
-        TraitSet tipDates = new TraitSetFromTaxonSet();
+
+        TraitSet tipDates = new TraitSet();
         tipDates.initByName(
                 "traitname", "date-forward",
                 "taxa", taxonSet,
-                "delimiter", "|",
-                "takeGroup", 1,
-                "dateFormat", "yyyy-M-dd");
+                "dateFormat", "yyyy-M-dd",
+                "value", "t1=2020-05-15," +
+                        "t2=2020-02-15," +
+                        "t3=2021-05-15," +
+                        "t4=2020-05-15," +
+                        "t5=2020-05-15");
 
-        TraitSet earlierBounds = new TraitSetFromTaxonSet();
+        TraitSet earlierBounds = new TraitSet();
         earlierBounds.initByName(
                 "traitname", "date-forward",
                 "taxa", taxonSet,
-                "delimiter", "|",
-                "takeGroup", 2,
-                "dateFormat", "yyyy-M-dd");
+                "dateFormat", "yyyy-M-dd",
+                "value", "t1=2020-05-01," +
+                        "t2=2020-02-01," +
+                        "t3=2021-05-01," +
+                        "t4=2020-05-14," +
+                        "t5=2020-05-14");
 
-        TraitSet laterBounds = new TraitSetFromTaxonSet();
+        TraitSet laterBounds = new TraitSet();
         laterBounds.initByName(
                 "traitname", "date-forward",
                 "taxa", taxonSet,
-                "delimiter", "|",
-                "takeGroup", 3,
-                "dateFormat", "yyyy-M-dd");
-
+                "dateFormat", "yyyy-M-dd",
+                "value", "t1=2020-06-01," +
+                        "t2=2020-03-01," +
+                        "t3=2021-06-01," +
+                        "t4=2020-05-15," +
+                        "t5=2020-05-15");
 
         Tree tree = new Tree();
         tree.initByName("taxonset", taxonSet,
