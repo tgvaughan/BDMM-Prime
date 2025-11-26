87350d741eff7fedc3012698b7009eaa7c496a5b
Tim Vaughan <timothy.vaughan@bsse.ethz.ch>
2025-11-26 13:57:56 +0100

    Updated copyright headers.
    Note: ETH owns copyright of material contributed by ETH authors.

--- src/bdmmprime/beauti/TypeTraitSetInputEditor.java
+++ src/bdmmprime/beauti/TypeTraitSetInputEditor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2024 ETH Zurich
+ * Copyright (C) 2019-2025 ETH Zurich
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -8,7 +8,7 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
