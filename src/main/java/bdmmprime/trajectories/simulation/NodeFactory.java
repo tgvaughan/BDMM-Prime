ca0e4d7c67aa225ceb1e56bf9a0e0f6cbcae649f
Tim Vaughan <timothy.vaughan@bsse.ethz.ch>
2026-05-07 16:59:45 +0200

    Updated copyright.

--- src/main/java/bdmmprime/trajectories/simulation/NodeFactory.java
+++ src/main/java/bdmmprime/trajectories/simulation/NodeFactory.java
@@ -1,18 +1,20 @@
 /*
- * Copyright (C) 2019-2025 ETH Zurich
+ * Copyright (c) 2017-2026 ETH Zürich
  *
- * This program is free software: you can redistribute it and/or modify
+ * This file is part of bdmm-prime.
+ *
+ * bdmm-prime is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * bdmm-prime is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with bdmm-prime. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package bdmmprime.trajectories.simulation;
